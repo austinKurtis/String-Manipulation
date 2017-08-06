b0799c2d0f297118c691b756dddb941c6d1713a5
AustinJ <austin.jay.kurtis@gmail.com>
2017-08-05 20:13:13 -0500

    working regExp

--- strings.js
+++ strings.js
@@ -1,7 +1,3 @@
-
-
-
-
 function reversal() {
 console.log("You got reversed Foo");
 	let backwardsMan = testString.split("").reverse().join("");
@@ -10,45 +6,58 @@ console.log("You got reversed Foo");
 }
 
 function alphabits() {
-	console.log("bits of Alpha");
+	console.log("Bits of Alpha");
 	let bitzed = testString.split("").sort().join("");
 	console.log(bitzed);
 	document.getElementById("bits").innerHTML = bitzed;
 }
 
 function palindrome() {
-console.log("Welcome to the Terror Dome");
+console.log("Welcome to the Terror Drome");
 let backwardsMan = testString.split("").reverse().join("");
 console.log("First Convert", backwardsMan);
 if (backwardsMan === testString){
-	document.getElementById("pali").innerHTML = testString + "is a palindrome!"
+	document.getElementById("pali").innerHTML = testString + "is a palindrome!";
 } else  {
-	document.getElementById("pali").innerHTML = testString + "is not a palindrome!"
+	document.getElementById("pali").innerHTML = testString + "is not a palindrome!";
 }
 
 }
 
 var testString = "";
-
 let stringButton = document.getElementById("btn-press");
 let stringInput = document.getElementById("input-box").value;
 
-
-
 function handle(e){
+
 	if(e.keyCode === 13){
 		e.preventDefault();
 		testString = document.getElementById("input-box").value;
+		let regExCheck = new RegExp("[^a-z|^A-Z]");
+		if (regExCheck.test(testString)) {
+			alert("Letters only, no numbers or special characters.");
+		} else {
 		console.log("TestString", testString);
 		reversal(testString);
 		alphabits(testString);
 		palindrome(testString);
+			}
 		}
 	}
+
+
+
 function sendTo() {
-	testString = document.getElementById("input-box").value;
-	console.log("TestString", testString);
-	reversal(testString);
-	alphabits(testString);
-	palindrome(testString);
-}
+		testString = document.getElementById("input-box").value;
+		let regExCheck = new RegExp("[^a-z|^A-Z]");
+		if (regExCheck.test(testString)) {
+			alert("Letters only, no numbers or special characters.");
+		} else {
+		console.log("TestString", testString);
+		reversal(testString);
+		alphabits(testString);
+		palindrome(testString);
+			}
+	}
+	
+
